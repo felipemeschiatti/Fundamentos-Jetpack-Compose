c35b8dd10990cce26114af9eca3c0c0c64b4c633
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-18 22:14:28 -0300

    adição do text.kt

--- app/src/main/java/felipemeschiatti/com/github/fundamentosjetpackcompose/MainActivity.kt
+++ app/src/main/java/felipemeschiatti/com/github/fundamentosjetpackcompose/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +29,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import felipemeschiatti.com.github.fundamentosjetpackcompose.ui.theme.FundamentosJetpackComposeTheme
@@ -29,7 +40,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FundamentosJetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize())
+                { innerPadding ->
 
                     CardMensagem(Modifier.padding(innerPadding))
                 }
@@ -95,30 +107,80 @@ class MainActivity : ComponentActivity() {
                 Text(text = "Conteúdo da mensagem")
             }
         }
+    }
 
-        @Preview(showBackground = true)
-        @Composable
-        fun PreviewFuncaoInicial() {
-            FundamentosJetpackComposeTheme {
-                Inicial()
-            }
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewFuncaoInicial() {
+        FundamentosJetpackComposeTheme {
+            //Inicial()
+            //TesteModifier()
+            TesteButtom()
         }
+    }
 
-        @Preview(showBackground = true)
-        @Composable
-        fun PreviewNotificationBage() {
-            FundamentosJetpackComposeTheme {
-                NotificationBage()
-            }
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewNotificationBage() {
+        FundamentosJetpackComposeTheme {
+            NotificationBage()
         }
+    }
 
-        @Preview(showBackground = true)
-        @Composable
-        fun PreviewCardMensagem() {
-            JetpackComposeTheme {
-                CardMensagem()
-            }
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewCardMensagem() {
+        FundamentosJetpackComposeTheme {
+            CardMensagem()
         }
+    }
+
+    @Composable
+    fun TesteModifier() {
+        Column {
+            Text(
+                text = "Item 1",
+                modifier = Modifier
+                    .border(border = BorderStroke(width = 1.dp, color = Color.Green))
+                    .padding(horizontal = 8.dp)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Item 1",
+                modifier = Modifier
+                    .border(border = BorderStroke(width = 1.dp, color = Color.Green))
+                    .padding(horizontal = 8.dp, vertical = 8.dp)
+            )
+
+            Text(
+                text = "Item 1",
+                modifier = Modifier
+
+                    .border(border = BorderStroke(width = 1.dp, color = Color.Green))
+                    .padding(horizontal = 8.dp)
+            )
+
+        }
+    }
+}
+        @Composable
+        fun TesteButtom() {
+            Button(
+                onClick = { /* TODO */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Green,
+                    contentColor = Color.Black
+                ),
+                shape = RoundedCornerShape(4.dp)
+            ) {
+                Text(text = "Confirmar")
+
+    }
 
     }
 
